b6a8f1eead6badd7995854cb71c08e6b995f86a3
rimi0610 <114567477+rimi0610@users.noreply.github.com>
2023-06-14 00:11:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,194 @@
+const categories=[
+  {
+      name:'All',
+      filter:'all',
+  },
+  {
+      name: 'Nature',
+      filter:'nature',
+  },
+  {
+      name:'Fruits',
+      filter:'fruits',
+  },
+  {
+      name:'Animals',
+      filter:'animals',
+  },
+  {
+      name:'Birds',
+      filter:'birds',
+  },
+  ];
+  const images =[
+      {
+          id:'1',
+          url:'../pictures/1.jpg',
+          category:'nature',
+          title:'1 Nature',
+      },
+      {
+          id:'2',
+          url:'../pictures/2.jpg',
+          category:'nature',
+          title:'2 nature',
+      },
+      {
+          id:'3',
+          url:'../pictures/3.jpg',
+          category:'nature',
+          title:'3 nature',
+      },
+      {
+          id:'4',
+          url:'../pictures/4.jpg',
+          category:'nature',
+          title:'4 nature',       
+      },
+      {
+          id:'5',
+          url:'../pictures/5.jpg',
+          category:'nature',
+          title:'5 nature',
+      },
+      {
+          id:'6',
+          url:'../pictures/6.jpg',
+          category:'nature',
+          title:'6 nature',
+      },
+      {
+          id:'7',
+          url:'../pictures/7.jpg',
+          category:'nature',
+          title:'7 nature',
+      },
+      {
+          id:'8',
+          url:'../pictures/8.jpg',
+          category:'birds',
+          title:'8 birdss',
+      },
+      {
+          id:'9',
+          url:'../pictures/9.jpg',
+          category:'birds',
+          title:'9 birds',
+      },
+      {
+          id:'10',
+          url:'../pictures/10.jpg',
+          category:'birds',
+          title:'10 birds',
+      },
+      {
+          id:'11',
+          url:'../pictures/11.jpg',
+          category:'birds',
+          title:'11 birds',
+      },
+      {
+          id:'12',
+          url:'../pictures/12.jpg',
+          category:'birds',
+          title:'12 birds',
+      },
+      {
+          id:'13',
+          url:'../pictures/13.jpg',
+          category:'birds',
+          title:'13 birds',
+      },
+      {
+          id:'14',
+          url:'../pictures/14.jpg',
+          category:'birds',
+          title:'14 birds',
+      },
+      {
+          id:'15',
+          url:'../pictures/15.jpg',
+          category:'animals',
+          title:'15 animals',
+      },
+      {
+          id:'16',
+          url:'../pictures/16.jpg',
+          category:'animals',
+          title:'16 animals',
+      },
+      {
+          id:'17',
+          url:'../pictures/17.jpg',
+          category:'animals',
+          title:'17 animals',
+      },
+      {
+        id:'18',
+        url:'../pictures/18.jpg',
+        category:'animals',
+        title:'18 animals',
+    },
+    {
+      id:'19',
+      url:'../pictures/19.jpg',
+      category:'animals',
+      title:'19 animals',
+  },
+  {
+    id:'20',
+    url:'../pictures/20.jpg',
+    category:'animals',
+    title:'20 animals',
+},
+{
+  id:'21',
+  url:'../pictures/21.jpg',
+  category:'animals',
+  title:'21 animals',
+},
+      {
+          id:'22',
+          url:'../pictures/22.jpg',
+          category:'fruits',
+          title:'22 fruits',
+      },
+      {
+          id:'23',
+          url:'../pictures/23.jpg',
+          category:'fruits',
+          title:'23 fruits',
+      },
+      {
+          id:'24',
+          url:'../pictures/24.jpg',
+          category:'fruits',
+          title:'24 fruits',
+      },
+      {
+          id:'25',
+          url:'../pictures/25.jpg',
+          category:'fruits',
+          title:'25 fruits',
+      },
+      {
+          id:'26',
+          url:'../pictures/26.jpg',
+          category:'fruits',
+          title:'26 fruits',
+      },
+      {
+          id:'27',
+          url:'../pictures/27.jpg',
+          category:'fruits',
+          title:'27 fruits',
+      },
+      {
+          id:'28',
+          url:'../pictures/28.jpg',
+          category:'fruits',
+          title:'28 fruits',
+      },
+
+
+  ];
